9ad74fdfd05be36b969293ee232b85f421e07f50
devarsh-13 <58954461+devarsh-13@users.noreply.github.com>
2021-04-04 15:11:03 +0530

    some work on chat...

--- js/show_users.js
+++ js/show_users.js
@@ -35,7 +35,7 @@ searchBar.onkeyup = ()=>{
 
 setInterval(() =>{
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", "php/users.php", true);
+  xhr.open("GET", "../Sem6CollegeProject/select_teachers.php", true);
   xhr.onload = ()=>{
     if(xhr.readyState === XMLHttpRequest.DONE){
         if(xhr.status === 200){
