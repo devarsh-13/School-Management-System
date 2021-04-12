6e2763cd5a69cf5d6ed524e2a53f4401de1d7655
devarsh-13 <58954461+devarsh-13@users.noreply.github.com>
2021-04-12 23:24:47 +0530

    Update main.php

--- main.php
+++ main.php
@@ -83,35 +83,11 @@ Purchase: http://themeforest.net/user/kamleshyadav
 	<!--Page main section start-->
 	<div id="educo_wrapper">
 		<!--Header start-->
-		<header id="ed_header_wrapper">
-			<div class="ed_header_bottom">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-2 col-md-2 col-sm-2">
-							<div class="educo_logo"> <a href="index.html"><img src="images/header/Logo.png" alt="logo" /></a> </div>
-						</div>
-						<div class="col-lg-8 col-md-8 col-sm-8">
-							<div class="edoco_menu_toggle navbar-toggle" data-toggle="collapse" data-target="#ed_menu">Menu <i class="fa fa-bars"></i>
-							</div>
-							<div class="edoco_menu">
-								<ul class="collapse navbar-collapse" id="ed_menu">
-									<li><a href="main.php">Home</a></li>
-									<li><a href="#event">events</a></li>
-									<li><a href="gallery.php">Gallery</a></li>
-									<li><a href="resource-download.php">Resources</a></li>
-									<li><a href="student_profile.php">Profile</a></li>
-									<li><a href="student_chat/select_users.php">Chat</a></li>
-									<li><a href="about.html">about us</a></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-lg-2 col-md-2 col-sm-2">
-							<div class="educo_call"><a href="#"><i class="fa fa-bell"></i>Notification</a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
+
+		<?php
+		include "header.php";
+		?>
+
 		<!--header end -->
 		<div class="ed_slider_form_section">
 			<!--Slider start-->
