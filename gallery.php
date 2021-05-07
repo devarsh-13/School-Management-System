a93be1e23a491de108cfce5cc23f1ecb1a809155
devarsh-13 <devarshantani13@gmail.com>
2021-05-07 22:57:16 +0530

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['s_id'])) {
     <!-- amchart css -->
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
     <!-- others css -->
-    <link rel="stylesheet" href="teacher/assets/css/typography.css">
+
     <!--<link rel="stylesheet" href="assets/css/default-css.css">-->
     <link rel="stylesheet" href="teacher/assets/css/styles.css">
     <link rel="stylesheet" href="teacher/assets/css/responsive.css">
